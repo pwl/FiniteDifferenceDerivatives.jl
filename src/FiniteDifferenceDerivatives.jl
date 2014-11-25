d7497f57be34b3db7b2234e49937f8b1e0357d95
Paweł Biernat <pawel.biernat+github@gmail.com>
2014-11-25 15:35:10 +0100

    Added fddat

--- src/FiniteDifferenceDerivatives.jl
+++ src/FiniteDifferenceDerivatives.jl
@@ -1,6 +1,6 @@
 module FiniteDifferenceDerivatives
 
-export fdd!, fdd, fddmatrix
+export fdd!, fdd, fddmatrix, fddat
 
 
 # returns a sparse matrix D such that D*u is a der-derivative of specified order
@@ -110,6 +110,67 @@ function fdd13!{T<:Number}(df::AbstractVector{T},x::AbstractVector{T},f::Abstrac
             h3=x[i+1]-x[i]
             df[i]=df1/h1 + (df3-df1)/(h1-h3) + df3/h3
         end
+    end
+end
+
+
+function fddat{T<:Number}(f::Vector{T},x::Vector{T},der::Int,x0::Real)
+    order = length(x)
+
+    if order != length(f)
+        error("x and f should have the same length")
+    end
+
+    N1 = 1
+    c    = zeros(T,order,der+1)
+    generatec!(c,x0,x,N1)
+
+    dfN = zero(T)
+    @simd for j = 1:order
+        @inbounds dfN += c[j,der+1]*f[N1+j-1]
+    end
+    return dfN
+end
+
+
+# generate the coefficients c
+function generatec!{T}(c::Matrix{T},x0::Real,x::Vector{T},N1::Int)
+    if N1 < 1 | N1 > length(x)
+        error("N1 is out of bounds")
+    end
+
+    order = size(c,1)
+    der   = size(c,2)-1
+
+    c1 = one(T)
+    c4 = x[N1] - x0
+    c[1] = one(T)
+    for i=1:order-1
+        mn = min(i,der)
+        c2 = one(T)
+        c5 = c4
+        @inbounds c4 = x[i+N1] - x0
+        j = zero(Int)
+        while j <= i-1
+            @inbounds c3 = x[i+N1] - x[j+N1]
+            c2 = c2*c3
+            if j == i-1
+                s = mn
+                while s >= 1
+                    @inbounds c[i+1,s+1] = c1*(s*c[i,s] - c5*c[i,s+1])/c2
+                    s-=1
+                end
+                @inbounds c[i+1,1] = -c1*c5*c[i,1]/c2
+            end
+            s = mn
+            while s >= 1
+                @inbounds c[j+1,s+1] = (c4*c[j+1,s+1] - s*c[j+1,s])/c3
+                s-=1
+            end
+            @inbounds c[j+1,1] = c4*c[j+1,1]/c3
+            j+=1
+        end
+        c1 = c2
     end
 end
 
